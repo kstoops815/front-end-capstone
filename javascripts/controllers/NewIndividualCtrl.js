08aa624c1cf6f9e3dfffb08494964667a5c9f218
Kelly Stoops <kstoops815@gmail.com>
2017-12-07 21:54:43 -0600

    gender selection dropdown working

--- javascripts/controllers/NewIndividualCtrl.js
+++ javascripts/controllers/NewIndividualCtrl.js
@@ -5,6 +5,7 @@ app.controller("NewIndividualCtrl", function($location, $scope, IndividualsServi
 	$scope.newIndividual = [];
 	$scope.button = "";
 	$scope.races = ["American Indian or Alaska Native", "Asian", "Black or African American", "Hispanic or Latino", "Native Hawaiian or Other Pacific Islander", "White"];
+	$scope.genders = ["female", "male", "transgender", "other"];
 
 	$scope.saveNewIndividual = (individual) => {
 		let newIndividual = IndividualsService.createIndividualObject(individual);
@@ -21,5 +22,8 @@ app.controller("NewIndividualCtrl", function($location, $scope, IndividualsServi
     	$scope.newIndividual.race = race;
   	};
 
+  	$scope.selectGender = (gender) => {
+    	$scope.newIndividual.gender = gender;
+  	};
 
 });
