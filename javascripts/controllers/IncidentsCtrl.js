c7dc3c23bf656361a798c25f5a050fb3955cc3fb
Kelly Stoops <kstoops815@gmail.com>
2017-12-09 14:01:55 -0600

    incidents printing to incident page

--- javascripts/controllers/IncidentsCtrl.js
+++ javascripts/controllers/IncidentsCtrl.js
@@ -1,5 +1,13 @@
 "use strict";
 
-app.controller("IncidentsCtrl", function($scope){
-	$scope.controller = "IncidentsCtrl";
+app.controller("IncidentsCtrl", function($scope, AuthService, IncidentsService){
+	const showIncidents = () => {
+		IncidentsService.getAllIncidents(AuthService.getCurrentUid()).then((results) => {
+			$scope.incidents = results;
+		}).catch((error) => {
+			console.log("error in showIncidents", error);
+		});
+	};
+
+	showIncidents();
 });
