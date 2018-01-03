ae2e87679ffa2d27e38b11245a562560d5c12264
Kelly Stoops <kstoops815@gmail.com>
2018-01-02 19:48:28 -0600

    code cleanup

--- javascripts/controllers/HomeCtrl.js
+++ javascripts/controllers/HomeCtrl.js
@@ -1,8 +1,6 @@
 "use strict";
 
 app.controller("HomeCtrl", function($location, $scope, AuthService, IncidentsService, IndividualsService){
-	// $scope.oneAtATimeIndividuals = true;
-	// $scope.oneAtATimeIncidents = true;
 	//Individiuals get, add, edit, and delete functions
 	const showIndividuals = () => {
 		IndividualsService.getAllIndividuals(AuthService.getCurrentUid()).then((results) => {
